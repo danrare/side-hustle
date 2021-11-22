331a56fd7d9de7fd7529b211d2e3d4de2a2e2954
danrare <61440086+danrare@users.noreply.github.com>
2021-11-22 07:52:37 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,4 @@
+document.getElementById("side-bar-drop").addEventListener("click", function () {
+    document.querySelector("#side-bar").classList.toggle("hide-bar");
+    document.getElementById("right-sec").classList.toggle("transparency");
+});
